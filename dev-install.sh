50b2e396898cc500e5c1eed74610370b5d014987
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2019-01-31 18:37:04 +0100

    Add jupyterlab-system-monitor extension

--- dev-install.sh
+++ dev-install.sh
@@ -14,3 +14,4 @@ yarn run build
 
 jupyter labextension link ./packages/jupyterlab-topbar --no-build
 jupyter labextension install ./packages/jupyterlab-topbar-extension
+jupyter labextension install ./packages/jupyterlab-system-monitor
